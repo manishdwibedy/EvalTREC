9f4c3403b485f8c915e30a88bcfb4d098b00ce24
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-18 23:29:01 -0700

    Can index documents in the solr instance.

---
+++ Solr/index.py
@@ -0,0 +1,20 @@
+import connection
+from util import constant
+
+def index(connection, collection, document):
+    """
+    Add the documents to the collection using the Solr connection.
+    :param connection: the solr connection
+    :param collection: the solr collection
+    :param document: the documents to be index
+    :return: Nothing
+    """
+    connection[collection].add(document)
+    connection[collection].commit()
+
+if __name__ == '__main__':
+    connection = connection.get_connection()
+    docs = [{"id":"1", "name":"a"},{"id":"2","name":"b"}]
+
+    index(connection, constant.SOLR_CORE,docs)
+
