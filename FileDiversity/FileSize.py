d0dca9cd460951e6cc7b493494adeec25bc1f233
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-18 22:47:06 -0700

    Modified the model. Now the mimeInfo is in an variable.

--- FileDiversity/FileSize.py
+++ FileDiversity/FileSize.py
@@ -1,17 +1,17 @@
 from MimeDiversity import MimeType
 
-class FileSize(MimeType.MIME):
+class FileSize(object):
     """
     The model class holding the filesize object
     """
-    def __init__(self, filename, mimeType, size):
-        MimeType.__init__(self, filename, mimeType)
+    def __init__(self, mimeObj, size):
+        self.mimeObj = mimeObj
         self.size = size
 
     def getInfo(self):
-        print "Filename - " + self.filename
-        print "MIME - " + self.mime
+        print "Filename - " + self.mimeObj.filename
+        print "MIME - " + self.mimeObj.mime
         print "size - " + self.size
 
     def __str__(self):
-        return "File - '%s' has a mime type of %s and file size of %s bytes" % (self.filename, self.mimeType, self.size)
+        return "File - '%s' has a mime type of %s and file size of %d bytes" % (self.mimeObj.filename, self.mimeObj.mimeType, self.size)
