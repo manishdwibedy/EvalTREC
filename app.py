17ad35f5a10b1008ba80d49ebc232f2275e380ba
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-19 23:57:30 -0700

    Loading the files seperately into the solr core.

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from FileDiversity import extractFileSizeDiversity
 from Solr.MIME import MIME_Core
 from ParserDiversity.getParserInfo import Parser
 from util.constant import DATA_DIR
+from SolrData import mimeData
 
 computeFileSize = False
 getDataFromSolr = True
+
 if __name__ == '__main__':
     mime = MIME_Core()
+
+    # Loading the files into solr
+    mimeData.MIME(DATA_DIR).loadData()
 
     if computeFileSize:
         file_size = extractFileSizeDiversity.getFileSizeInfo()
