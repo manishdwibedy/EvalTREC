c732a80de7d5864f0fd537237b755b1404bba654
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-19 23:33:42 -0700

    Getting the meta data from the parser module.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from FileDiversity import extractFileSizeDiversity
 from Solr.MIME import MIME_Core
+from ParserDiversity.getParserInfo import Parser
+from util.constant import DATA_DIR
 
-
+computeFileSize = False
+getDataFromSolr = True
 if __name__ == '__main__':
     mime = MIME_Core()
 
-    file_size = extractFileSizeDiversity.getFileSizeInfo()
-    mime.index(file_size)
+    if computeFileSize:
+        file_size = extractFileSizeDiversity.getFileSizeInfo()
+        mime.index(file_size)
+
+    parser = Parser(DATA_DIR, getDataFromSolr)
+    parser.getMetaData()
