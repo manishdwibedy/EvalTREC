dee88f84c2df69f2119d45b782730a0a72dabed7
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-19 23:34:32 -0700

    Currently just printing the byte size of the meta data.

--- ParserDiversity/getParserInfo.py
+++ ParserDiversity/getParserInfo.py
@@ -1,15 +1,28 @@
-from Tika import tika
+from Tika import getTika
 from util import utility
+from Solr.MIME import MIME_Core
+import sys
 
 class Parser(object):
 
-    def __init__(self, directory):
+    def __init__(self, directory, readFromSolr):
         self.directory = directory
+        self.readFromSolr = readFromSolr
 
     def getMetaData(self):
         MIMEList = []
 
-        # Getting all the files in the directory
-        files = utility.getFilesInDirectory(self.directory)
-        print 'Got', str(len(files)), 'file(s).'
+        if not self.readFromSolr:
+            # Reading all the files in the directory
+            files = utility.getFilesInDirectory(self.directory)
+            print 'Got', str(len(files)), 'file(s).'
 
+        tikaObj = getTika.Tika()
+        response = MIME_Core().query('*:*')
+        files = response.result.dict['response']['docs']
+        for file in files:
+            filelocation = file['filename'][0]
+            metadata = tikaObj.getMetaData(filelocation)
+            print metadata
+            print sys.getsizeof(metadata)
+            pass
