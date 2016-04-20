192a40deea021de7d706b1aef68ff14f1065d8ea
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-20 00:25:14 -0700

    Calling the file size module from the main script.

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from FileDiversity import extractFileSizeDiversity
-
 # SOLR Mime Core
 from Solr.MIME import MIME_Core
 
 # Importing the data directory
 from util.constant import DATA_DIR
 
+# MIME Modue
 from MimeDiversity import ComputeMIME
+
 # Parser Module
 from ParserDiversity.getParserInfo import Parser
 
 # The files to be loaded into solr
 from SolrData import mimeData
 
+from FileDiversity import ComputeFileSize
+
 # Various Flags to control which module would run
-computeFileSize = False
+computeFileSize = True
 getDataFromSolr = True
 resetSolrData = True
 commuteMIME = True
@@ -32,8 +34,8 @@ if __name__ == '__main__':
         ComputeMIME.GetMIMEInformation().addMime()
         pass
     if computeFileSize:
-        file_size = extractFileSizeDiversity.getFileSizeInfo()
-        mime.index(file_size)
+        size = ComputeFileSize.FileSize().addSize()
+        pass
 
     parser = Parser(DATA_DIR, getDataFromSolr)
     parser.getMetaData()
