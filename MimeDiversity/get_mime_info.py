4152c77ada6e5a6021adccf0cb596e251a7b2fcc
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-20 00:04:59 -0700

    Removing the script. Would be combining all the scripts into one app.py file.

--- MimeDiversity/get_mime_info.py
+++
@@ -1,8 +0,0 @@
-from MimeDiversity import ComputeMIME
-from util import utility
-
-if __name__ == '__main__':
-    mime = ComputeMIME.GetMIMEInformation(utility.constant.DATA_DIR)
-    mimeInfo = mime.computeMIME()
-
-    print mimeInfo
