bbcce1c0a9374da67148811c28b625f78306040f
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-22 01:15:07 -0700

    Considering that the unit can be of two words as well. Also the units can be made of alphabets only.

--- MeasurementDiversity/RegexTester.py
+++ MeasurementDiversity/RegexTester.py
@@ -7,7 +7,7 @@ class MeasurementRegex():
         Creates the regex to be used to get measurements
         :return:
         """
-        self.regex = r"(\d+(\.\d+)?)\s*(\w+)"
+        self.regex = r"(\d+(\.\d+)?)(\s*([a-zA-Z]+))(\s*(\w[a-zA-Z]+))?"
 
     def isMeasurement(self, string):
         """
@@ -27,19 +27,33 @@ class MeasurementRegex():
         """
         Gets the measurement from the string
         :param string: the string to be search
-        :return: the tuple containing the measure and unit, if the string contains a measurement,
+        :return: the list of object containing the measure and unit, if the string contains a measurement,
         otherwise None is returned
         """
+        measurementList = []
+
         if(self.isMeasurement(string)):
-            matches = re.search(self.regex, string)
-            measure = matches.group(1)
-            unit = matches.group(3)
-            return measure, unit
+            matches = re.findall(self.regex, string)
+            for match in matches:
+                if not match[2].isdigit():
+                    pass
+                if len(match) == 6:
+                    measure = {
+                        'measure': match[0],
+                        'unit': match[3] + ' ' + match[5]
+                    }
+                else:
+                    measure = {
+                        'measure': match[0],
+                        'unit': match[3]
+                    }
+                measurementList.append(measure)
+            return measurementList
         else:
             return None
 
 
 if __name__ == '__main__':
-    string = 'assaa asdasd cvxvxcv adsasd asddasd'
+    string = 'Steve Bloom reporting from 2014AG asd 10 M asd11 asd '
     print MeasurementRegex().isMeasurement(string)
     print MeasurementRegex().getMeasurement(string)
