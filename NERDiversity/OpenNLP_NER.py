8e6d049e87d1f99aad6429705a745a21a7f65ea4
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-30 12:18:07 -0700

    Extracting NER from a file.

---
+++ NERDiversity/OpenNLP_NER.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+
+def extract_NER(file_name):
+    '''
+    Getting the NER concepts of a single file
+    :param file_name: the absolute file name of the file
+    :return: the NER object
+    '''
+    headers = {
+                'Content-Disposition': 'attachment',
+                'filename': '0A0C0478C62A4BF29F47B5783CBD2CEB12DCF27E5BCD56A4871AFF84FB0C5DD2',
+                'Accept': 'application/json'
+              }
+
+    with open(file_name) as fh:
+        mydata = fh.read()
+        response = requests.put("http://localhost:9998/meta", headers=headers, data=mydata, params={'filename': headers['filename']})
+        json_output = response.text
+
+
+    obj = json.loads(json_output)
+
+    NER_data = {}
+    for key, value in obj.iteritems():
+        key = str(key)
+        if key.startswith('NER_'):
+            NER_concepts = []
+
+            for NER_concept in value:
+                NER_concept = str(NER_concept)
+                NER_concepts.append(NER_concept)
+
+            NER_data[key[4:]] = NER_concepts
+
+    return NER_data
+
+if __name__ == "__main__":
+
+    file_name = '/Users/manishdwibedy/PycharmProjects/MIME/Data/html/Training/0A0C0478C62A4BF29F47B5783CBD2CEB12DCF27E5BCD56A4871AFF84FB0C5DD2'
+    print extract_NER(file_name)
