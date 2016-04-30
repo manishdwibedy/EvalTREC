01c8520d83aa6a3db2729c1bd2941d869f14bf2b
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-30 12:22:32 -0700

    Extracting the file name from the file location.

--- NERDiversity/OpenNLP_NER.py
+++ NERDiversity/OpenNLP_NER.py
@@ -1,20 +1,22 @@
 import requests
 import json
+import os.path
 
-
-def extract_NER(file_name):
+def extract_NER(file_location):
     '''
     Getting the NER concepts of a single file
-    :param file_name: the absolute file name of the file
+    :param file_location: the absolute file name of the file
     :return: the NER object
     '''
+
+    file_name = os.path.basename(file_location)
     headers = {
                 'Content-Disposition': 'attachment',
-                'filename': '0A0C0478C62A4BF29F47B5783CBD2CEB12DCF27E5BCD56A4871AFF84FB0C5DD2',
+                'filename': file_name,
                 'Accept': 'application/json'
               }
 
-    with open(file_name) as fh:
+    with open(file_location) as fh:
         mydata = fh.read()
         response = requests.put("http://localhost:9998/meta", headers=headers, data=mydata, params={'filename': headers['filename']})
         json_output = response.text
