bb9392fefd1d0b14bd1706da1e0af653336b024e
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-01 15:58:39 -0700

    Computing the file size diversity.

---
+++ FileDiversity/visualization.py
@@ -0,0 +1,44 @@
+from util import utility
+from Solr.MIME import MIME_Core
+
+
+class Visulization(object):
+
+    def __init__(self):
+        self.SOLR = MIME_Core()
+        self.sizes = utility.constant.FILE_SIZE
+        self.size_mapping = utility.constant.SIZE_MAPPING
+
+    def extractMIME(self):
+        FileSizeList = []
+
+        # Getting the files whose size would be computed
+        response = MIME_Core().facetQuery('metadata')
+        mimeTypeResponse = response.result.dict['facet_counts']['facet_fields']['metadata']
+
+        mimeList = []
+        for mime_type, count in mimeTypeResponse.iteritems():
+            mimeList.append(mime_type)
+
+
+        mime_size_diversity = {}
+        for mime in mimeList:
+            jsonObj = {}
+            for size in self.sizes:
+                query = 'metadata:%s AND size:[%s]' % (mime, size)
+                # metadata:*plain* AND size:[* TO 10000]
+                response = MIME_Core().queryAll(query=query)
+                files = response.result.dict['response']['docs']
+                jsonObj[self.size_mapping[size]] = [len(files)]
+            mime_size_diversity[mime] = jsonObj
+        pass
+
+
+    def addSize(self):
+        size = self.extractMIME()
+        print '\n\nStarting to index the filesize of the dataset'
+        self.SOLR.index(size)
+        print '\nIndexed!'
+
+if __name__ == '__main__':
+    Visulization().extractMIME()
--- util/constant.py
+++ util/constant.py
@@ -6,3 +6,19 @@ DATA_DIR = '/Users/manishdwibedy/PycharmProjects/MIME/final/'
 SOLR_HOSTNAME = 'localhost'
 SOLR_PORT = 8983
 SOLR_CORE = 'mime'
+
+FILE_SIZE = [
+    '0 TO 10000',
+    '10000 TO 1000000',
+    '1000000 TO 10000000',
+    '10000000 TO 100000000',
+    '100000000 TO *'
+]
+
+SIZE_MAPPING = {
+    '0 TO 10000' : '0 - 10 KB',
+    '10000 TO 1000000' : '10 KB - 1 MB',
+    '1000000 TO 10000000' : '1 MB - 10 MB',
+    '10000000 TO 100000000' : '10 MB - 100 MB',
+    '100000000 TO *' : '100 MB+'
+}
