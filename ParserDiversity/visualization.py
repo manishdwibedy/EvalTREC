1982b4dfc5a49ee31d05b5c3f212284f0e1f1a6d
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-02 16:08:34 -0700

    Preparing the JSON files - (metadata size and content size) to be used in the visualizations.

---
+++ ParserDiversity/visualization.py
@@ -0,0 +1,56 @@
+from util import utility
+from Solr.MIME import MIME_Core
+import json
+
+class Visulization(object):
+
+    def __init__(self):
+        self.SOLR = MIME_Core()
+        self.sizes = utility.constant.FILE_SIZE
+        self.size_mapping = utility.constant.SIZE_MAPPING
+
+    def extractParsingInfo(self):
+        FileSizeList = []
+
+        # Getting the files whose size would be computed
+        response = MIME_Core().facetQuery('metadata')
+        mimeTypeResponse = response.result.dict['facet_counts']['facet_fields']['metadata']
+
+
+        mimeList = []
+        for mime_type, count in mimeTypeResponse.iteritems():
+            mimeList.append(mime_type)
+
+
+        mime_size_diversity = {}
+        for mime in mimeList:
+            metadata_size = {}
+            content_size = {}
+            print mime[mime.index('/')+1:]
+
+            for size in self.sizes:
+                query = 'metadata:%s AND size:[%s]' % (mime, size)
+                response = MIME_Core().queryAll(query=query)
+                files = response.result.dict['response']['docs']
+
+                avg_content_size = 0
+                avg_metadata_size = 0
+                for file in files:
+                    avg_metadata_size += file['metadata_size'][0]
+                    avg_content_size += file['content_size'][0]
+
+                if len(files) != 0:
+                    metadata_size[self.size_mapping[size]] = [avg_metadata_size / len(files)]
+                    content_size[self.size_mapping[size]] = [avg_content_size / len(files)]
+                else:
+                    metadata_size[self.size_mapping[size]] = 0
+                    content_size[self.size_mapping[size]] = 0
+            out_file = open('data/metadata/'+mime[mime.index('/')+1:]+'.json',"w")
+            json.dump(metadata_size,out_file, indent=4)
+
+            out_file = open('data/content/'+mime[mime.index('/')+1:]+'.json',"w")
+            json.dump(content_size,out_file, indent=4)
+
+
+if __name__ == '__main__':
+    Visulization().extractParsingInfo()
