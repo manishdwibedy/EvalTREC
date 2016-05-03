a8bb407d48f1b994296d42911fbd218dac07e3aa
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-02 18:10:40 -0700

    Adding these units to the constant variable - UNITS

--- util/constant.py
+++ util/constant.py
@@ -24,3 +24,5 @@ SIZE_MAPPING = {
     '10000000 TO 100000000' : '10 MB - 100 MB',
     '100000000 TO *' : '100 MB+'
 }
+
+UNITS = ['ft','feet','Euro','f','minutes','miles','beta','mm','degrees','years','cm','M','square meters','megabytes','Mbytes','AU','mm','acres','acres','m','months','degree Fahrenheit','Epoch','nm','degrees','meters','pm','T','watts','days','epsilon','years','megahertz','meter','million km','megawatts','minute','Mg','Mb','km','mintues','kilometers','GB','ft','years','acres','mb','m','km','m','kilometer','moons','million hectares','hPa','cm','calories','inch','kilograms','size','UTC','minute','KH','JT','mile','F','pennies','ton','hours','day','deg','hours','km','kilogram','years','m','nanometers','days','Tg','Tg','F','pounds','kB','hour','hectares','km','bytes','degrees Celcius','db','sq km','nm','km','pm','MB','GHz','m','mb','microns','F','meter','mm','km','HFC','km','minutes','mph','pounds','square feet','ft','mm','impressions','micron','inch','kilogram','mL','miles','gallons','area','inch','years','km','mm','Ph','Pi','volts','minutes','m','hour','miles','Gigabytes','degree','m']
